0f0be0ed4b912f4041b6ec47033e07a5d4532978
NMcKenzie77 <n.mckenzie@registechllc.com>
2026-06-27 07:19:30 -0400

    Allow upgraded agent route to register

--- src/portal.ts
+++ src/portal.ts
@@ -2,7 +2,6 @@ import express, { type Express } from 'express';
 
 const proto = express.application as unknown as {
   get: Function;
-  listen: Function;
   __portalRoutePatch?: boolean;
 };
 
@@ -10,20 +9,17 @@ if (!proto.__portalRoutePatch) {
   proto.__portalRoutePatch = true;
 
   const originalGet = proto.get;
-  const originalListen = proto.listen;
-  let skipBasicAgentRoutes = true;
 
   proto.get = function patchedGet(this: Express, path: unknown, ...handlers: unknown[]) {
-    if (skipBasicAgentRoutes && (path === '/agent' || path === '/agent/login')) return this;
-    return originalGet.call(this, path, ...handlers);
-  };
+    const isAgentPath = path === '/agent' || path === '/agent/login';
+    const handlerText = handlers.map((handler) => String(handler)).join('\n');
 
-  proto.listen = function patchedListen(this: Express, ...args: unknown[]) {
-    skipBasicAgentRoutes = false;
-    return originalListen.apply(this, args);
+    if (isAgentPath && handlerText.includes('agentPageHtml')) return this;
+
+    return originalGet.call(this, path, ...handlers);
   };
 }
 
 await import('./u2.js');
 
-export const portalVersion = '4';
+export const portalVersion = '5';
