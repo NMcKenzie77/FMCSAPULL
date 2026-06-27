c0f81e0cc07b328d1104871dac96fb5376565652
NMcKenzie77 <n.mckenzie@registechllc.com>
2026-06-27 07:08:49 -0400

    Fix agent portal route registration

--- src/portal.ts
+++ src/portal.ts
@@ -1,16 +1,29 @@
 import express, { type Express } from 'express';
-import './u2.js';
 
-const proto = express.application as unknown as { get: Function; __portalRoutePatch?: boolean };
+const proto = express.application as unknown as {
+  get: Function;
+  listen: Function;
+  __portalRoutePatch?: boolean;
+};
 
 if (!proto.__portalRoutePatch) {
   proto.__portalRoutePatch = true;
+
   const originalGet = proto.get;
+  const originalListen = proto.listen;
+  let skipBasicAgentRoutes = true;
 
   proto.get = function patchedGet(this: Express, path: unknown, ...handlers: unknown[]) {
-    if (path === '/agent' || path === '/agent/login') return this;
+    if (skipBasicAgentRoutes && (path === '/agent' || path === '/agent/login')) return this;
     return originalGet.call(this, path, ...handlers);
+  };
+
+  proto.listen = function patchedListen(this: Express, ...args: unknown[]) {
+    skipBasicAgentRoutes = false;
+    return originalListen.apply(this, args);
   };
 }
 
-export const portalVersion = '3';
+await import('./u2.js');
+
+export const portalVersion = '4';
