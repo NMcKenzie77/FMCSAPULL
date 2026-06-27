5c2765c28bcfbcf54630697e3f34694ba14b0128
NMcKenzie77 <n.mckenzie@registechllc.com>
2026-06-27 07:04:50 -0400

    Let portal overlay replace basic agent route

--- src/portal.ts
+++ src/portal.ts
@@ -1,3 +1,16 @@
+import express, { type Express } from 'express';
 import './u2.js';
 
-export const portalVersion = '2';
+const proto = express.application as unknown as { get: Function; __portalRoutePatch?: boolean };
+
+if (!proto.__portalRoutePatch) {
+  proto.__portalRoutePatch = true;
+  const originalGet = proto.get;
+
+  proto.get = function patchedGet(this: Express, path: unknown, ...handlers: unknown[]) {
+    if (path === '/agent' || path === '/agent/login') return this;
+    return originalGet.call(this, path, ...handlers);
+  };
+}
+
+export const portalVersion = '3';
